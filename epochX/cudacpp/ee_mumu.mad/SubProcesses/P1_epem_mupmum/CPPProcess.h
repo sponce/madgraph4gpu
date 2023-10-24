83f866bb0655d5343bf39c6e4e3e12da75ff7fe7
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2023-10-24 18:19:15 +0200

    cleaned up CPPProcess.h

--- epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_epem_mupmum/CPPProcess.h
+++ epochX/cudacpp/ee_mumu.mad/SubProcesses/P1_epem_mupmum/CPPProcess.h
@@ -12,8 +12,7 @@
 // Visit launchpad.net/madgraph5 and amcatnlo.web.cern.ch
 //==========================================================================
 
-#ifndef MG5_Sigma_sm_epem_mupmum_H
-#define MG5_Sigma_sm_epem_mupmum_H 1
+#pragma once
 
 #include "mgOnGpuConfig.h"
 
@@ -23,38 +22,23 @@
 
 #include <vector>
 
-//--------------------------------------------------------------------------
-
-#ifdef __CUDACC__
-namespace mg5amcGpu
-#else
-namespace mg5amcCpu
-#endif
-{
+namespace mg5amcCpu {
   //==========================================================================
   // A class for calculating the matrix elements for
   // Process: e+ e- > mu+ mu- WEIGHTED<=4 @1
   //--------------------------------------------------------------------------
 
-  class CPPProcess
-  {
+  class CPPProcess {
   public: /* clang-format off */
 
     // Constructor (from command line arguments)
     CPPProcess( bool verbose = false, bool debug = false );
 
     // Destructor
-    ~CPPProcess();
+    virtual ~CPPProcess();
 
     // Initialize process (read model parameters from file)
     virtual void initProc( const std::string& param_card_name );
-
-    // Other methods of this instance (???)
-    //const std::vector<fptype>& getMasses() const { return m_masses; }
-    //virtual int code() const{ return 1; }
-    //void setInitial( int inid1, int inid2 ){ id1 = inid1; id2 = inid2; }
-    //int getDim() const { return dim; }
-    //int getNIOParticles() const { return nexternal; } // nexternal was nioparticles
 
     // Accessors (unused so far: add four of them only to fix a clang build warning)
     //bool verbose() const { return m_verbose; }
@@ -72,19 +56,6 @@ namespace mg5amcCpu
     static constexpr int npar = npari + nparf; // #particles in total (external = initial + final): e.g. 4 for e+ e- -> mu+ mu-
     static constexpr int ncomb = 16; // #helicity combinations: e.g. 16 for e+ e- -> mu+ mu- (2**4 = fermion spin up/down ** npar)
 
-    // Hardcoded parameters for this process (constant class variables)
-    // [NB: this class assumes nprocesses==1 i.e. a single DSIG1 and no DSIG2 in Fortran (#272 and #343)]
-    // [NB: these parameters (e.g. nwf) are P1-specific, i.e. they are different for different P1 subdirectories (#644)]
-    // [NB: I am currently unable to get the right value of nwf in CPPProcess.h - will hardcode it in CPPProcess.cc instead (#644)]
-    //static const int nwf = ??; // #wavefunctions = #external (npar) + #internal: e.g. 5 for e+ e- -> mu+ mu- (1 internal is gamma or Z)
-
-    // Other variables of this instance (???)
-    //static const int ninitial = CPPProcess::npari;
-    //static const int nexternal = 4; // CPPProcess::npar (nexternal was nioparticles)
-    //static const int nwavefuncs = 6; // (?!?! this should be nwf but export_cpp gives the wrong value here)
-    //static const int namplitudes = 2;
-    //static const int ncomb = 16; // CPPProcess::ncomb
-
   private: /* clang-format on */
 
     // Command line arguments (constructor)
@@ -94,90 +65,36 @@ namespace mg5amcCpu
     // Physics model parameters to be read from file (initProc function)
     Parameters_sm* m_pars;
     std::vector<fptype> m_masses; // external particle masses
-
-    // Other variables of this instance (???)
-    //int id1, id2; // initial particle ids
-    //cxtype** amp; // ???
   };
 
-  //--------------------------------------------------------------------------
-
-#ifdef __CUDACC__
-  __global__ void
-  computeDependentCouplings( const fptype* allgs,    // input: Gs[nevt]
-                             fptype* allcouplings ); // output: couplings[nevt*ndcoup*2]
-#else
   __global__ void
   computeDependentCouplings( const fptype* allgs,  // input: Gs[nevt]
                              fptype* allcouplings, // output: couplings[nevt*ndcoup*2]
                              const int nevt );     // input: #events (for cuda: nevt == ndim == gpublocks*gputhreads)
-#endif
 
-  //--------------------------------------------------------------------------
-
-#ifdef __CUDACC__ /* clang-format off */
   __global__ void
   sigmaKin_getGoodHel( const fptype* allmomenta,   // input: momenta[nevt*npar*4]
                        const fptype* allcouplings, // input: couplings[nevt*ndcoup*2]
                        fptype* allMEs,             // output: allMEs[nevt], |M|^2 final_avg_over_helicities
-#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
                        fptype* allNumerators,      // output: multichannel numerators[nevt], running_sum_over_helicities
                        fptype* allDenominators,    // output: multichannel denominators[nevt], running_sum_over_helicities
-#endif
-                       bool* isGoodHel );          // output: isGoodHel[ncomb] - device array (CUDA implementation)
-#else
-  __global__ void
-  sigmaKin_getGoodHel( const fptype* allmomenta,   // input: momenta[nevt*npar*4]
-                       const fptype* allcouplings, // input: couplings[nevt*ndcoup*2]
-                       fptype* allMEs,             // output: allMEs[nevt], |M|^2 final_avg_over_helicities
-#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-                       fptype* allNumerators,      // output: multichannel numerators[nevt], running_sum_over_helicities
-                       fptype* allDenominators,    // output: multichannel denominators[nevt], running_sum_over_helicities
-#endif
                        bool* isGoodHel,            // output: isGoodHel[ncomb] - host array (C++ implementation)
                        const int nevt );           // input: #events (for cuda: nevt == ndim == gpublocks*gputhreads)
-#endif /* clang-format on */
-
-  //--------------------------------------------------------------------------
 
   int                                           // output: nGoodHel (the number of good helicity combinations out of ncomb)
   sigmaKin_setGoodHel( const bool* isGoodHel ); // input: isGoodHel[ncomb] - host array
 
-  //--------------------------------------------------------------------------
-
-#ifdef __CUDACC__ /* clang-format off */
   __global__ void
   sigmaKin( const fptype* allmomenta,      // input: momenta[nevt*npar*4]
             const fptype* allcouplings,    // input: couplings[nevt*ndcoup*2]
             const fptype* allrndhel,       // input: random numbers[nevt] for helicity selection
             const fptype* allrndcol,       // input: random numbers[nevt] for color selection
             fptype* allMEs,                // output: allMEs[nevt], |M|^2 final_avg_over_helicities
-#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
             const unsigned int channelId,  // input: multichannel channel id (1 to #diagrams); 0 to disable channel enhancement
             fptype* allNumerators,         // output: multichannel numerators[nevt], running_sum_over_helicities
             fptype* allDenominators,       // output: multichannel denominators[nevt], running_sum_over_helicities
-#endif
-            int* allselhel,                // output: helicity selection[nevt]
-            int* allselcol                 // output: helicity selection[nevt]
-            );
-#else
-  __global__ void
-  sigmaKin( const fptype* allmomenta,      // input: momenta[nevt*npar*4]
-            const fptype* allcouplings,    // input: couplings[nevt*ndcoup*2]
-            const fptype* allrndhel,       // input: random numbers[nevt] for helicity selection
-            const fptype* allrndcol,       // input: random numbers[nevt] for color selection
-            fptype* allMEs,                // output: allMEs[nevt], |M|^2 final_avg_over_helicities
-#ifdef MGONGPU_SUPPORTS_MULTICHANNEL
-            const unsigned int channelId,  // input: multichannel channel id (1 to #diagrams); 0 to disable channel enhancement
-            fptype* allNumerators,         // output: multichannel numerators[nevt], running_sum_over_helicities
-            fptype* allDenominators,       // output: multichannel denominators[nevt], running_sum_over_helicities
-#endif
             int* allselhel,                // output: helicity selection[nevt]
             int* allselcol,                // output: helicity selection[nevt]
             const int nevt );              // input: #events (for cuda: nevt == ndim == gpublocks*gputhreads)
-#endif /* clang-format on */
 
-  //--------------------------------------------------------------------------
 }
-
-#endif // MG5_Sigma_sm_epem_mupmum_H
