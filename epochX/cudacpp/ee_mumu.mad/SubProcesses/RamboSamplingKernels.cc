ae55ef58120e03a8f270caaa9c6d30df52bc1288
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2023-10-29 21:55:38 +0100

    cleaning getMomentaFinal

--- epochX/cudacpp/ee_mumu.mad/SubProcesses/RamboSamplingKernels.cc
+++ epochX/cudacpp/ee_mumu.mad/SubProcesses/RamboSamplingKernels.cc
@@ -60,24 +60,13 @@ namespace mg5amcCpu {
   }
 
   void RamboSamplingKernelHost::getMomentaFinal() {
+    const fptype twopi = 8. * atan( 1. );
+    const fptype po2log = log( twopi / 4. );
     for( size_t ievt = 0; ievt < nevt(); ievt+=neppV ) {
       for( size_t ieppM = 0; ieppM < neppV; ++ieppM ) {
-        // AOSOA[ipagM][0][0][ieppM]
-        const fptype* rndmom =  &( m_rndmom.data()[ievt * nparf * np4 + ieppM] );
-        fptype* momenta = &( m_momenta.data()[ievt * npar * np4 + ieppM] );
-        fptype& wt = m_weights.data()[ievt+ieppM];
-
-        // initialization step: factorials for the phase space weight
-        const fptype twopi = 8. * atan( 1. );
-        const fptype po2log = log( twopi / 4. );
-        fptype z[nparf];
-        z[1] = po2log;
-        for( int kpar = 2; kpar < nparf; kpar++ )
-          z[kpar] = z[kpar - 1] + po2log - 2. * log( fptype( kpar - 1 ) );
-        for( int kpar = 2; kpar < nparf; kpar++ )
-          z[kpar] = ( z[kpar] - log( fptype( kpar ) ) );
 
         // generate n massless momenta in infinite phase space
+        const fptype* rndmom =  &( m_rndmom.data()[ievt * nparf * np4 + ieppM] );
         fptype q[nparf][np4];
         for( int iparf = 0; iparf < nparf; iparf++ ) {
           const fptype r1 = rndmom[(iparf * 4    ) * 8];
@@ -94,8 +83,7 @@ namespace mg5amcCpu {
         }
 
         // calculate the parameters of the conformal transformation
-        fptype r[np4];
-        for( int i4 = 0; i4 < np4; i4++ ) r[i4] = 0.;
+        fptype r[np4] = {0., 0., 0., 0.};
         for( int iparf = 0; iparf < nparf; iparf++ ) {
           for( int i4 = 0; i4 < np4; i4++ ) r[i4] = r[i4] + q[iparf][i4];
         }
@@ -107,6 +95,7 @@ namespace mg5amcCpu {
         const fptype x0 = m_energy / rmas;
 
         // transform the q's conformally into the p's (i.e. the 'momenta')
+        fptype* momenta = &( m_momenta.data()[ievt * npar * np4 + ieppM] );
         for( int iparf = 0; iparf < nparf; iparf++ ) {
           fptype bq = b[0] * q[iparf][1] + b[1] * q[iparf][2] + b[2] * q[iparf][3];
           for( int i4 = 1; i4 < np4; i4++ ) {
@@ -116,8 +105,8 @@ namespace mg5amcCpu {
         }
 
         // calculate weight (NB return log of weight)
+        fptype& wt = m_weights.data()[ievt+ieppM];
         wt = po2log;
-        if( nparf != 2 ) wt = ( 2. * nparf - 4. ) * log( m_energy ) + z[nparf - 1];
 
         // issue warnings if weight is too small or too large
         static int iwarn[5] = { 0, 0, 0, 0, 0 };
